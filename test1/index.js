ab681c06a01ba81ddbb891b18c5ab71de007071d
touH <visitor.tou@gmail.com>
2020-07-02 11:44:15 +0800

    mod: 前端缓存学习

---
+++ test1/index.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const fs = require('fs')
+const { join } = require('path')
+
+http.createServer((req, res) => {
+  if(req.url === '/index.html') {
+    console.log('请求index.html')
+    res.setHeader('Cache-control', 'max-age=10')
+    fs.readFile(join(__dirname, 'index.html'), (err, data) => {
+      res.end(data)
+    })
+  }
+  if(req.url === '/a.js') {
+    console.log('请求a.js')
+    res.setHeader('Cache-control', 'max-age=10')
+    fs.readFile(join(__dirname, '../a.js'), (err, data) => {
+      res.end(data)
+    })
+  }
+  if(req.url === '/a.css') {
+    console.log('请求a.css')
+    res.setHeader('Cache-control', 'max-age=10')
+    fs.readFile(join(__dirname, '../a.css'), (err, data) => {
+      res.end(data)
+    })
+  }
+  if(req.url === '/a.jpg') {
+    console.log('请求a.jpg')
+    res.setHeader('Cache-control', 'max-age=10')
+    fs.readFile(join(__dirname, '../a.jpg'), (err, data) => {
+      res.end(data)
+    })
+  }
+}).listen(5000)
